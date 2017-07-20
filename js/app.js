ce2ad1db6491b15cf1378ceece715c1aa9a28021
karolina <carolha.es@gmail.com>
2017-07-20 14:07:14 +0200

    score board

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+
     const square = document.querySelectorAll(".board div");
-
-    const colors = ["red", "blue", "yellow", "green"];
-
+    const colors = ["red", "blue", "yellow", "green", "white", "hotpink"];
 
     for (let i = 0; i < square.length; i++) {
         square[i].addEventListener("mouseover", function() {
-            let index = Math.floor(Math.random() * 3) + 1;
+            let index = Math.floor(Math.random() * colors.length);
+            console.log(index);
             this.style.backgroundColor = colors[index];
         })
     }
 
+    const scoreBoard = document.querySelector(".scoreBoard");
+    const button = document.querySelector(".check");
+    console.log(button);
+
+    button.addEventListener("click", function() {
+        console.log(this);
+    })
 
 
 
