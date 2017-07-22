1cc75526445fb3e792d7c929964996f4239490c8
karolina <carolha.es@gmail.com>
2017-07-22 15:38:08 +0200

    counting points almost ready, to be added black squares doesn't add points only

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GameOfColors
 quick and easy game to kill some time
+
+additional board adds one color at a time, not everything at once
+
+
+add conditioin so the black background doesnt add points
+
+//future features
+next level game try to copy the pattern shown in board game
--- js/app.js
+++ js/app.js
@@ -2,51 +2,71 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // const board = document.querySelector(".board");
     const square = document.querySelectorAll(".board div");
+    //basic board
     const colors = ["red", "blue", "yellow"];
-    const additional = ["purlpe", "green", "white", "hotpink"];
+    const levelUp = ["purple", "green", "white", "hotpink"];
+    const button = document.querySelector(".check");
 
-    //
+    let level = 1;
+
+    //kolorowanie divów przez event
     for (let i = 0; i < square.length; i++) {
         square[i].addEventListener("mouseover", function() {
+            event.preventDefault();
             let index = Math.floor(Math.random() * colors.length);
             this.style.backgroundColor = colors[index];
             this.setAttribute("color", colors[index]);
             let x = this.getAttribute("color");
-            // console.log(this , x);
         })
     }
 
-    const scoreBoard = document.querySelector(".scoreBoard");
-    const button = document.querySelector(".check");
-
-    button.addEventListener("click", function() {
-        const arr = [];
+    //reset kolorowania po zakońćzonej rozgrywce
+    function reset() {
         for (let i = 0; i < square.length; i++) {
-            let col = square[i].getAttribute("color");
-            arr.push(col);
+            square[i].style.backgroundColor = "black";
         }
-        // console.log(arr);
+    }
 
-        function changeColor() {
-            const html = document.querySelector("html");
-            html.style.backgroundColor = "navy";
-        }
+    //change backgroundColor
+    function changeColor() {
+        const html = document.querySelector("html");
+        html.style.backgroundColor = "navy";
+    }
 
-        for (let i = 0; i < arr.length; i++) {
-            if ( arr[0] === arr[1] && arr[0] === arr[2] && arr[0] === arr[3]
-                && arr[0] === arr[4] && arr[0] === arr[5] && arr[0] === arr[6]
-                && arr[0] === arr[7] && arr[0] === arr[8]){
-                //add color if board finished
-                colors.push.apply(colors,additional);
-                return changeColor();
-            }else{
-                return false;
+    //check if it is correct
+    button.addEventListener("click", function() {
+        event.preventDefault();
+            const arr = [];
+            for (let i = 0; i < square.length; i++) {
+                let color = square[i].getAttribute("color");
+                arr.push(color);
             }
-        }
+
+            for (let i = 0; i < arr.length; i++) {
+
+                if (arr[0] === arr[1] && arr[0] === arr[2] && arr[0] === arr[3]
+                    && arr[0] === arr[4] && arr[0] === arr[5] && arr[0] === arr[6]
+                    && arr[0] === arr[7] && arr[0] === arr[8]){
+                    //add color and increase level if board is finished
+                    colors.push.apply(colors,levelUp);
+                    reset();
+                    changeColor();
+                }else{
+                    return false;
+                }
+            }
+            // level++;
+            console.log(level, "level po buttonie");
+            const result = document.querySelector(".result");
+            level = level + 1;
+            result.innerHTML = level;
+
+            return level;
+    });
 
 
 
-    })
+
 
 
 
