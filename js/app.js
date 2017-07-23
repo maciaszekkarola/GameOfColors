a9182b9b0209f580123d89ee5368410ba7a07f0e
karolina <carolha.es@gmail.com>
2017-07-23 12:11:56 +0200

    miniboard is working correctly, styling and adding closure to the game left

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // const board = document.querySelector(".board");
     const square = document.querySelectorAll(".board div");
-    //basic board
     const colors = ["red", "blue", "yellow"];
     const level2 = ["purple"];
     const level3 = ["green"];
@@ -14,9 +13,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const middleSq = document.querySelector(".middle");
     const miniBoard = document.querySelector(".miniBoard");
     const miniSquare = document.querySelectorAll(".miniBoard div");
-
-    console.log(miniSquare);
-
 
     let level = 1;
     result.innerHTML = level;
@@ -48,51 +44,88 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //miniBoard randomColor
     function colorMiniBoard() {
+        const miniArr = [];
         for (let i = 0; i < miniSquare.length; i++) {
             let index = Math.floor(Math.random() * colors.length);
             miniSquare[i].style.backgroundColor = colors[index];
             miniSquare[i].setAttribute("color", colors[index]);
+            let miniSquareColor = miniSquare[i].getAttribute("color");
+            miniArr.push(miniSquareColor);
         }
+        return miniArr;
     }
+
     colorMiniBoard();
+    const miniColorSet = colorMiniBoard();
+
+
+
+    // main game function
+    function game() {
+        const arr = [];
+
+        for (let i = 0; i < square.length; i++) {
+            let color = square[i].getAttribute("color");
+            arr.push(color);
+        }
+
+        if (level === 2) {
+            colors.push.apply(colors,level2);
+        }else if (level === 3) {
+            colors.push.apply(colors,level3);
+        }else if (level === 4) {
+            colors.push.apply(colors,level4);
+        }else if (level === 5) {
+            colors.push.apply(colors, level5);
+        }else if (level > 5){
+            miniBoard.style.visibility = "visible";
+            equal();
+            reset();
+            changeColor();
+            colorMiniBoard();
+            level++;
+        }else if (level === 6){
+            alert("Go back to work!");
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[0] === arr[1] && arr[0] === arr[2] && arr[0] === arr[3]
+                && arr[0] === arr[4] && arr[0] === arr[5] && arr[0] === arr[6]
+                && arr[0] === arr[7] && arr[0] === arr[8]){
+                reset();
+                changeColor();
+            }else{
+                return false;
+            }
+        }
+        level++;
+        result.innerHTML = level;
+        console.log(arr);
+
+        //rules from level 5 up
+        function equal(){
+            for(let i = 0 ; i < arr.length; i++){
+                for(let j = 0; j < miniColorSet.length; j++){
+                    if( arr[i] === miniColorSet[i]){
+                        console.log("hiphip!");
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+    return arr;
+    }
+
+
 
     //main event
     button.addEventListener("click", function(event) {
         event.preventDefault();
-            const arr = [];
-            for (let i = 0; i < square.length; i++) {
-                let color = square[i].getAttribute("color");
-                arr.push(color);
-            }
-
-            if (level === 1) {
-                colors.push.apply(colors,level2);
-            }else if (level === 2) {
-                colors.push.apply(colors,level3);
-            }else if (level === 3) {
-                colors.push.apply(colors,level4);
-            }else if (level === 4) {
-                colors.push.apply(colors, level5);
-            }else if ( level >= 5){
-                miniBoard.style.visibility = "visible";
-
-            }
-
-            for (let i = 0; i < arr.length; i++) {
-                if (arr[0] === arr[1] && arr[0] === arr[2] && arr[0] === arr[3]
-                    && arr[0] === arr[4] && arr[0] === arr[5] && arr[0] === arr[6]
-                    && arr[0] === arr[7] && arr[0] === arr[8]){
-                    reset();
-                    changeColor();
-                }else{
-                    return false;
-                }
-            }
-            // console.log(colors);
-            level++;
-            result.innerHTML = level;
-        return level;
+        game();
     });
+    const bigColorSet = game();
+
 
 
 
